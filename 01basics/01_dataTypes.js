5296d8d66c54612ea27f7528b1ccb6f7a7828301
Piyush Shinde <55225174+piyush-721@users.noreply.github.com>
2023-12-10 16:41:14 +0000

    datatype

---
+++ 01basics/01_dataTypes.js
@@ -0,0 +1,21 @@
+
+
+// code readability must be high
+
+let age = 24;
+let isLoggedIn = false;
+let state = null;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
